674df44c640f1224ad9f6e278fab65153d37ad2b
Lyndon Panton <LyndonPanton@gmail.com>
2019-05-25 20:09:27 +0100

    Added logic for checking file input

--- js/app.js
+++ js/app.js
@@ -22,6 +22,26 @@
 	});
 
 	function checkInput(input) {
-		
+		// If a file has been received
+		if (input.name) {
+			// If the file has the mp3, ogg, wav or webm extension
+            if (input.name.slice(-3) == "mp3" || input.name.slice(-3) == "ogg" || input.name.slice(-3) == "wav" || input.name.slice(-4) == "webm") {
+                document.getElementById("form-file-label").textContent = "You have selected a valid file";
+                document.getElementById("feedback-name").textContent = input.name;
+                document.getElementById("feedback-size").textContent = input.size + " bytes";
+                document.getElementById("feedback-type").textContent = input.type;
+                return true;
+            } else {
+                document.getElementById("form-file-label").textContent = "Please select an audio file with the mp3/ogg/webm/wav extension";
+                document.getElementById("feedback-name").textContent = input.name;
+                document.getElementById("feedback-size").textContent = input.size + " bytes";
+                document.getElementById("feedback-type").textContent = input.type;
+                return false;
+            }
+        } else {
+            document.getElementById("form-file-label").textContent = "You have not selected a file";
+            document.getElementById("feedback-name").textContent = "N/A";
+            return false;
+        }
 	}
 }());
