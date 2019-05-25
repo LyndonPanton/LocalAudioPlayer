d24cbfcc2e8a514fbb97c5b7fe12b615a5e86f92
Lyndon Panton <LyndonPanton@gmail.com>
2019-05-25 20:03:22 +0100

    Pressing the enter key on the label triggers click on file input

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 (function() {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
+
+	let formFieldLabel = document.getElementById("form-file-label");
+	formFieldLabel.addEventListener("keydown", function(event) {
+		if (event.keyCode == "13") {
+			document.getElementById("form-file").click();
+		}
+	});
 }());
